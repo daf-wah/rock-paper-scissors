e3fdd3c6213afdd001ca14b2f33cf4b4dd294ab1
Dafwah <joshua@fineartportrait.org>
2023-05-06 09:23:51 -0500

    Begin Prelim Code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// const getPlayerOneInput = prompt("Rock, Paper, or Scissors?");
+
+function playerOne() {
+  const playerOneSelection = getPlayerOneInput.toLowerCase();
+  return playerOneSelection;
+}
+
+const getCPUInput = ["Rock", "Paper", "Scissors"];
+
+function cpuInput() {
+  const getRandomNumber = Math.trunc(Math.random() * 3);
+  const cpuPick = getCPUInput[getRandomNumber];
+  const cpuSelection = cpuPick.toLowerCase();
+  return cpuSelection;
+}
+
+function startGame() {
+  playerOne();
+  cpuInput();
+
+  let result = 
+}
