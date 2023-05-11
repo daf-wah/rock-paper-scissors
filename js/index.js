70533d04c9d3e2d57a0cba2539c8c9fafd3e34a1
Dafwah <joshua@fineartportrait.org>
2023-05-11 12:01:42 -0500

    Modify Rock/Paper/Scissor Buttons

--- js/index.js
+++ js/index.js
@@ -1,12 +1,78 @@
 "use strict";
 
-function playerOne() {
-  const getPlayerOneInput = prompt("Rock, Paper, or Scissors?");
-  const playerOneSelection = getPlayerOneInput.toLowerCase();
-  return playerOneSelection;
-}
+const result = document.querySelector(".result");
+const player = document.querySelector(".player");
+const cpu = document.querySelector(".cpu");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+let playerNum = 0;
+let cpuNum = 0;
+player.textContent = `Player: ${playerNum}`;
+cpu.textContent = `Computer: ${cpuNum}`;
+
+// let rockPlay = rock.addEventListener("click", function () {
+//   const cpuSelection = cpuInput();
+//   const playerSelect = "rock";
+
+//   if (playerSelect === cpuSelection) {
+//     result.textContent = "We have a tie!";
+//   } else if (cpuSelection === "paper") {
+//     result.textContent = `The CPU chose paper. CPU Wins!`;
+//     cpu.textContent = `Computer: ${++cpuNum}`;
+//   } else if (cpuSelection === "scissors") {
+//     result.textContent = `The CPU chose scissors. You Win!`;
+//     player.textContent = `Player: ${++playerNum}`;
+//   }
+// });
+
+// let paperPlay = paper.addEventListener("click", function () {
+//   const cpuSelection = cpuInput();
+//   const playerSelect = "paper";
+
+//   if (playerSelect === cpuSelection) {
+//     result.textContent = "We have a tie!";
+//   } else if (cpuSelection === "scissors") {
+//     result.textContent = `The CPU chose scissors. CPU Wins!`;
+//     cpu.textContent = `Computer: ${++cpuNum}`;
+//   } else if (cpuSelection === "rock") {
+//     result.textContent = `The CPU chose rock. You Win!`;
+//     player.textContent = `Player: ${++playerNum}`;
+//   }
+// });
+
+// let scissorsPlay = scissors.addEventListener("click", function () {
+//   const cpuSelection = cpuInput();
+//   const playerSelect = "scissors";
+
+//   if (playerSelect === cpuSelection) {
+//     result.textContent = "We have a tie!";
+//   } else if (cpuSelection === "rock") {
+//     result.textContent = `The CPU chose rock. CPU Wins!`;
+//     cpu.textContent = `Computer: ${++cpuNum}`;
+//   } else if (cpuSelection === "paper") {
+//     result.textContent = `The CPU chose paper. You Win!`;
+//     player.textContent = `Player: ${++playerNum}`;
+//   }
+// });
+
+let rockPlay = rock.addEventListener("click", function () {
+  let playedRock = "rock";
+  return playedRock;
+});
+
+let paperPlay = paper.addEventListener("click", function () {
+  let playedPaper = "paper";
+  return playedPaper;
+});
+
+let scissorsPlay = scissors.addEventListener("click", function () {
+  let playedScissors = "scissors";
+  return playedScissors;
+});
 
 const getCPUInput = ["Rock", "Paper", "Scissors"];
+const getPlayerInput = [rockPlay, paperPlay, scissorsPlay];
 
 function cpuInput() {
   const getRandomNumber = Math.trunc(Math.random() * 3);
@@ -16,40 +82,30 @@ function cpuInput() {
 }
 
 function startGame() {
+  result.textContent = `Let's play!`;
   for (let i = 5; i > 0; i--) {
-    const playerOneSelection = playerOne();
-    const cpuSelection = cpuInput();
+    let playerInput = getPlayerInput;
+    let cpuInput = cpuInput();
 
-    if (playerOneSelection === cpuSelection) {
-      console.log(
-        `Player One: ${playerOneSelection} / CPU: ${cpuSelection}  We have a draw!`
-      );
-    } else if (playerOneSelection === "rock" && cpuSelection === "paper") {
-      console.log(
-        `Player One: ${playerOneSelection} / CPU: ${cpuSelection}  CPU Wins!`
-      );
-    } else if (playerOneSelection === "rock" && cpuSelection === "scissors") {
-      console.log(
-        `Player One: ${playerOneSelection} / CPU: ${cpuSelection}  Player One Wins!`
-      );
-    } else if (playerOneSelection === "paper" && cpuSelection === "rock") {
-      console.log(
-        `Player One: ${playerOneSelection} / CPU: ${cpuSelection}  Player One Wins!`
-      );
-    } else if (playerOneSelection === "paper" && cpuSelection === "scissors") {
-      console.log(
-        `Player One: ${playerOneSelection} / CPU: ${cpuSelection}  CPU Wins!`
-      );
-    } else if (playerOneSelection === "scissors" && cpuSelection === "rock") {
-      console.log(
-        `Player One: ${playerOneSelection} / CPU: ${cpuSelection}  CPU Wins!`
-      );
-    } else if (playerOneSelection === "scissors" && cpuSelection === "paper") {
-      console.log(
-        `Player One: ${playerOneSelection} / CPU: ${cpuSelection}  Player One Wins!`
-      );
+    if (playerInput === cpuInput) {
+      result.textContent = `We have a tie!`;
+    } else if (
+      (playerInput === `rock` && cpuInput === `scissors`) ||
+      (playerInput === `paper` && cpuInput === `rock`) ||
+      (playerInput === `scissors` && cpuInput === `paper`)
+    ) {
+      result.textContent = `The CPU chose ${cpuInput}. You win!`;
+      player.textContent = `Player: ${++playerNum}`;
+    } else {
+      result.textContent = `The CPU chose ${cpuInput}. You lose!`;
+      cpu.textContent = `Computer: ${++cpuNum}`;
+    }
+
+    if (playerNum === 5) {
+      result.textContent = `You win the series!`;
+    } else if (cpuNum === 5) {
+      result.textContent = `The CPU has defeated you!`;
     }
   }
 }
-
-// startGame();
+startGame();
